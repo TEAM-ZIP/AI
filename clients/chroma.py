6f402643277befc6a242b9e9daf3c80a71bbf78a
yongaricode <kang061313@gmail.com>
2025-05-08 08:49:18 +0900

    fix: ChromaDB 종류 변경

--- clients/chroma.py
+++ clients/chroma.py
@@ -1,10 +1,7 @@
-# clients/chroma.py
-import chromadb
-from chromadb.config import Settings
+from chromadb import PersistentClient
 
-# 전역에서 재사용할 클라이언트
-chroma_client = chromadb.Client(Settings(persist_directory="./chroma_store"))
+# ✅ check_chroma.py와 동일한 방식으로 통일
+chroma_client = PersistentClient(path="./chroma_store")
 
-# 필요한 컬렉션도 여기서 정의
 book_collection = chroma_client.get_or_create_collection("book_embeddings")
 profile_collection = chroma_client.get_or_create_collection("user_profiles")
