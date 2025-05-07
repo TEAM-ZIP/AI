f18678a2a4735d82e1ee2aff957323cb7e3d60f4
yongaricode <kang061313@gmail.com>
2025-05-08 08:47:43 +0900

    fix: 임베딩 저장 경로 오류 수정

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from routes.chat import router as chat_router
 from routes.bookEmbedding import router as embedding_router
 from routes.userProfile import router as profile_router
 from fastapi import FastAPI
-from chromadb import PersistentClient
-
 
 load_dotenv()
 
-
-chroma_client = PersistentClient(path="./chroma_db")
 
 app = FastAPI()
 
